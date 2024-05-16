b53e80f58f0895e9228b019e642b04f7a1dcc7c3
Fabian Bätscher (fba) <fabian.baetscher@datahouse.ch>
2024-05-16 14:41:07 +0200

    initial commit

---
+++ frontend/src/component/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <label htmlFor="name"></label>
+      <input name="name"></input>
+    </div>
+  );
+};
+
+export default Login;
