303d3efb4ce6119debfb74b9bf7465bf9192c7fa
Fabian275 <108061531+Fabian275@users.noreply.github.com>
2024-07-04 15:49:05 +0200

    Pwfilter (#4)
    * fixed maxID
    
    * filtering
    
    * added category and fixed updating pws
    
    * delete function added
    
    * unterteilen der passwörter auf mehrere seiten
    
    ---------
    
    Co-authored-by: Fabian Bätscher (fba) <fabian.baetscher@datahouse.ch>

--- frontend/src/components/PasswordManager.tsx
+++ frontend/src/components/PasswordManager.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -8,30 +8,37 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import {
   Button,
+  FormControl,
   IconButton,
   InputAdornment,
   InputLabel,
+  MenuItem,
   OutlinedInput,
+  Select,
   TextField,
+  TablePagination,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Password, Visibility, VisibilityOff } from "@mui/icons-material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import Nav from "./Nav";
 
 interface PasswordData {
+  pwId: number;
+  category: string;
   link: string;
   username: string;
   password: string;
 }
 
 interface Props {
-  setLoggedIn: Dispatch<SetStateAction<boolean>>;
+  setLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const PasswordManager = (props: Props) => {
   const { setLoggedIn } = props;
   const navigate = useNavigate();
+  const [category, setCategory] = useState("");
   const [link, setLink] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -39,24 +46,39 @@ const PasswordManager = (props: Props) => {
   const [visiblePasswords, setVisiblePasswords] = useState<{
     [key: string]: boolean;
   }>({});
-  const [showPassword, setShowPassword] = React.useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [sortBy, setSortBy] = useState("link");
+  const [order, setOrder] = useState("desc");
+  const [filterCategory, setFilterCategory] = useState("");
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [totalItems, setTotalItems] = useState(0);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const fetchPasswords = async () => {
+  const fetchPasswords = async (page: number, rowsPerPage: number) => {
     try {
       const response = await axios.get("http://localhost:5001/getPasswords", {
         withCredentials: true,
+        params: {
+          sortBy: sortBy,
+          order: order,
+          filterCategory: filterCategory,
+          page: page + 1,
+          pageSize: rowsPerPage,
+        },
       });
-      setPasswords(response.data);
+      setPasswords(response.data.passwords);
+      setTotalItems(response.data.totalItems);
     } catch (error) {
       console.error("Fehler beim Holen der Passwörter:", error);
     }
   };
 
   useEffect(() => {
-    fetchPasswords();
-  }, []);
+    fetchPasswords(page, rowsPerPage);
+  }, [sortBy, order, filterCategory, page, rowsPerPage]);
 
   const togglePasswordVisibility = (link: string) => {
     setVisiblePasswords((prevState) => ({
@@ -70,26 +92,64 @@ const PasswordManager = (props: Props) => {
   };
 
   const addNewPW = async (e: any) => {
-    console.log(e);
     try {
-      if (link !== "" && username !== "" && password !== "") {
+      if (
+        link !== "" &&
+        username !== "" &&
+        password !== "" &&
+        category !== ""
+      ) {
         const response = await axios.post(
           "http://localhost:5001/addNewPassword",
           {
             link,
             username,
             password,
+            category,
           },
           { withCredentials: true }
         );
-        fetchPasswords();
+        fetchPasswords(page, rowsPerPage);
         setLink("");
         setUsername("");
         setPassword("");
+        setCategory("");
       }
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const handleSort = (column: string) => {
+    const newOrder = sortBy === column && order === "asc" ? "desc" : "asc";
+    setSortBy(column);
+    setOrder(newOrder);
+  };
+
+  const handleDelete = async (pwId: number) => {
+    try {
+      const response = await axios.delete(
+        "http://localhost:5001/deletePassword",
+        {
+          data: { pwId },
+          withCredentials: true,
+        }
+      );
+      fetchPasswords(page, rowsPerPage);
+    } catch (error) {
+      console.log("konnte nicht löschen", error);
+    }
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   return (
@@ -101,10 +161,56 @@ const PasswordManager = (props: Props) => {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Link</TableCell>
-              <TableCell>Benutzername</TableCell>
-              <TableCell>Passwort</TableCell>
+              <TableCell>
+                <FormControl fullWidth>
+                  <InputLabel id="category-select-label">Kategorie</InputLabel>
+                  <Select
+                    labelId="category-select-label"
+                    id="category-select"
+                    label="Kategorie hinzufügen"
+                    value={filterCategory}
+                    onChange={(e) => setFilterCategory(e.target.value)}
+                  >
+                    <MenuItem value={""}>--- kein Filter ---</MenuItem>
+                    <MenuItem value={"Websites"}>Websites</MenuItem>
+                    <MenuItem value={"Software-Lizenzen"}>
+                      Software-Lizenzen
+                    </MenuItem>
+                    <MenuItem value={"Bank"}>Bank</MenuItem>
+                    <MenuItem value={"E-Mail-Konten"}>E-Mail-Konten</MenuItem>
+                    <MenuItem value={"Soziale Medien"}>Soziale Medien</MenuItem>
+                    <MenuItem value={"WLAN-Passwörter"}>
+                      WLAN-Passwörter
+                    </MenuItem>
+                    <MenuItem value={"Identitäten"}>Identitäten</MenuItem>
+                    <MenuItem value={"Sicherheitsfragen"}>
+                      Sicherheitsfragen
+                    </MenuItem>
+                    <MenuItem value={"Notizen"}>Notizen</MenuItem>
+                    <MenuItem value={"Telefonnummern"}>Telefonnummern</MenuItem>
+                    <MenuItem value={"Arbeitskonten"}>Arbeitskonten</MenuItem>
+                    <MenuItem value={"Streaming-Dienste"}>
+                      Streaming-Dienste
+                    </MenuItem>
+                    <MenuItem value={"Gesundheitskonten"}>
+                      Gesundheitskonten
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+              </TableCell>
+              <TableCell onClick={() => handleSort("link")}>
+                Link {sortBy === "link" && (order === "asc" ? "▲" : "▼")}
+              </TableCell>
+              <TableCell onClick={() => handleSort("username")}>
+                Benutzername
+                {sortBy === "username" && (order === "asc" ? "▲" : "▼")}
+              </TableCell>
+              <TableCell onClick={() => handleSort("password")}>
+                Passwort
+                {sortBy === "password" && (order === "asc" ? "▲" : "▼")}
+              </TableCell>
               <TableCell>Anpassen</TableCell>
+              <TableCell>Löschen</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -112,6 +218,42 @@ const PasswordManager = (props: Props) => {
               key="newEntry"
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
+              <TableCell component="th" scope="row" sx={{ minWidth: 200 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="category-select-label">Kategorie</InputLabel>
+                  <Select
+                    labelId="category-select-label"
+                    id="category-select"
+                    label="Kategorie hinzufügen"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                  >
+                    <MenuItem value={"Websites"}>Websites</MenuItem>
+                    <MenuItem value={"Software-Lizenzen"}>
+                      Software-Lizenzen
+                    </MenuItem>
+                    <MenuItem value={"Bank"}>Bank</MenuItem>
+                    <MenuItem value={"E-Mail-Konten"}>E-Mail-Konten</MenuItem>
+                    <MenuItem value={"Soziale Medien"}>Soziale Medien</MenuItem>
+                    <MenuItem value={"WLAN-Passwörter"}>
+                      WLAN-Passwörter
+                    </MenuItem>
+                    <MenuItem value={"Identitäten"}>Identitäten</MenuItem>
+                    <MenuItem value={"Sicherheitsfragen"}>
+                      Sicherheitsfragen
+                    </MenuItem>
+                    <MenuItem value={"Notizen"}>Notizen</MenuItem>
+                    <MenuItem value={"Telefonnummern"}>Telefonnummern</MenuItem>
+                    <MenuItem value={"Arbeitskonten"}>Arbeitskonten</MenuItem>
+                    <MenuItem value={"Streaming-Dienste"}>
+                      Streaming-Dienste
+                    </MenuItem>
+                    <MenuItem value={"Gesundheitskonten"}>
+                      Gesundheitskonten
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+              </TableCell>
               <TableCell component="th" scope="row">
                 <TextField
                   id="outlined-basic"
@@ -158,12 +300,16 @@ const PasswordManager = (props: Props) => {
                   Hinzufügen
                 </Button>
               </TableCell>
+              <TableCell component="th" scope="row" />
             </TableRow>
-            {passwords.map((row, index) => (
+            {passwords.map((row) => (
               <TableRow
-                key={index}
+                key={row.pwId}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
+                <TableCell component="th" scope="row">
+                  {row.category}
+                </TableCell>
                 <TableCell component="th" scope="row">
                   {row.link}
                 </TableCell>
@@ -177,15 +323,32 @@ const PasswordManager = (props: Props) => {
                 <TableCell>
                   <Button
                     variant="contained"
-                    onClick={() => navigate(`/update-profile/${index}`)}
+                    onClick={() => navigate(`/update-profile/${row.pwId}`)}
                   >
                     Ändern
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  <Button
+                    variant="contained"
+                    onClick={() => handleDelete(row.pwId)}
+                  >
+                    Löschen
                   </Button>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
+        <TablePagination
+          rowsPerPageOptions={[10, 20, 30]}
+          component="div"
+          count={totalItems}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </TableContainer>
     </div>
   );
